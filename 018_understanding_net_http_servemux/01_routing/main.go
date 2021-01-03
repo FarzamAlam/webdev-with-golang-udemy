18d7d56a525b71912b25c56f35772d88695e1036
FarzamAlam <farzamcse@gmail.com>
2021-01-03 21:23:58 +0530

    Implemented simple router

---
+++ 018_understanding_net_http_servemux/01_routing/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+type hotdog int
+
+func (h hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/dog":
+		io.WriteString(w, "Barking...")
+	case "/cat":
+		io.WriteString(w, "Meow...")
+	default:
+		io.WriteString(w, "Silent Animal")
+	}
+}
+func main() {
+	var h hotdog
+	http.ListenAndServe(":8080", h)
+}
