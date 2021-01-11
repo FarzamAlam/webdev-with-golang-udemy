311fc7b9e45104cfcb52bf4497733ee97e67e819
FarzamAlam <farzamcse@gmail.com>
2021-01-11 23:54:21 +0530

    Added routes in third party mux

--- 021_third_serve_mux/main.go
+++ 021_third_serve_mux/main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	mux := httprouter.New()
+
+	mux.GET("/", index)
+	mux.GET("/about", about)
+	mux.GET("/contact", contact)
+	mux.GET("/apply", apply)
+	mux.POST("/apply/", applyProcess)
+	mux.GET("/user/:name", user)
+	mux.GET("/blog/:category/:article", blogWrite)
+	http.ListenAndServe(":8080", mux)
+}
+
+func user(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "USER: %s!\n", ps.ByName("name"))
+}
+
+func blogRead(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "WRITE CATEGORY, %s!\n", ps.ByName("category"))
+	fmt.Fprintf(w, "WRITE ARTICLE, %s!\n", ps.ByName("article"))
 }
