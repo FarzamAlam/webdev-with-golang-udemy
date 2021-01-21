243e065e19e7615a96be25af6c448632c25ef94c
FarzamAlam <farzamcse@gmail.com>
2021-01-21 19:07:13 +0530

    Implemented hands on 7

---
+++ 022_hands_on/07/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("Error while listening: %v\n", err)
+	}
+
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go serve(conn)
+	}
+}
+
+func serve(c net.Conn) {
+	defer c.Close()
+	scanner := bufio.NewScanner(c)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if ln == "" {
+			fmt.Println("THIS IS THE END OF THE HTTP REQUEST HEADER.")
+			break
+		}
+	}
+}
