0f5bd57840ecc5eddb67873cceecadeedfd9854f
FarzamAlam <farzamcse@gmail.com>
2021-01-22 21:47:53 +0530

    Implement hands on 8

---
+++ 022_hands_on/08/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		serve(conn)
+	}
+}
+func serve(c net.Conn) {
+	defer c.Close()
+	scanner := bufio.NewScanner(c)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if ln == "" {
+			fmt.Println("THIS IS THE END OF THE HTTP REQUEST HEADERS.")
+			break
+		}
+	}
+	body := "CHECK OUT THE RESPONSE BODY PAYLOAD"
+	io.WriteString(c, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(c, "Content-Lenght: %d\r\n", len(body))
+	fmt.Fprintf(c, "Content-Type: text/plain\r\n")
+	io.WriteString(c, "\r\n")
+	io.WriteString(c, body)
+}
